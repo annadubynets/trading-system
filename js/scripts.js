023d1259ed6d5dc4ccbb3659ab7c5e9603bfe9d9
Anna Dubynets <arogulina@gmail.com>
2021-10-12 13:16:21 +0300

    fix sidebar

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,37 @@
-$(document).ready(function() {
+// $(document).ready(function() {
 
-    $('#sidebarCollapse').on('click', function() {
-        $('#sidebar').toggleClass('active');
-    });
+//     $('#sidebarCollapse').on('click', function() {
+//         $('#sidebar').toggleClass('active');
+//     });
 
+// });
+
+// $(document).ready(function() {
+//     $('.toggle-button').on('click', function() {
+
+//         $('.animated-icon').toggleClass('open');
+//     });
+// });
+
+let sidebar = document.querySelector(".sidebar");
+let closeBtn = document.querySelector("#btn");
+let searchBtn = document.querySelector(".bx-search");
+
+closeBtn.addEventListener("click", () => {
+    sidebar.classList.toggle("open");
+    menuBtnChange(); //calling the function(optional)
 });
 
-$(document).ready(function() {
-    $('.toggle-button').on('click', function() {
-
-        $('.animated-icon').toggleClass('open');
-    });
+searchBtn.addEventListener("click", () => { // Sidebar open when you click on the search icon
+    sidebar.classList.toggle("open");
+    menuBtnChange(); //calling the function(optional)
 });
+
+// following are the code to change sidebar button(optional)
+function menuBtnChange() {
+    if (sidebar.classList.contains("open")) {
+        closeBtn.classList.replace("bx-menu", "bx-menu-alt-right"); //replacing the icons class
+    } else {
+        closeBtn.classList.replace("bx-menu-alt-right", "bx-menu"); //replacing the icons class
+    }
+}
